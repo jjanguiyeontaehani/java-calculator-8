f320ff8f0c29f50e2a9c202ac40ea0cee492a6b8
BadAss <never4575@naver.com>
2025-10-19 01:56:23 +0900

    fix(ConsoleView): fix typo in CUSTOM_SEPARATOR_SUFFIX

--- src/main/java/calculator/CalculatorConfig.java
+++ src/main/java/calculator/CalculatorConfig.java
@@ -3,5 +3,5 @@ package calculator;
 public class CalculatorConfig {
     public static final String DEFAULT_SEPERATOR_REGEX = ",|:";
     public static final String CUSTOM_SEPERATOR_PREFIX = "//";
-    public static final String CUSTOM_SEPERATOR_SUFFIX = "\n";
+    public static final String CUSTOM_SEPERATOR_SUFFIX = "\\n";
 }
